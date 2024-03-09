a592f6373cef91bcd3cf7a6d46222cc08a058d2f
EzekielBanda <bsc-com-10-19@unima.ac.mw>
2024-03-09 10:27:48 +0200

    added configuration files

---
+++ app/src/main/java/com/unitech/learning/ui/theme/Color.kt
@@ -0,0 +1,43 @@
+package com.unitech.learning.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PurpleStart = Color(0xFFD24BE9)
+val PurpleEnd = Color(0xFFDF8FEC)
+
+val BlueStart = Color(0xFF2196F3)
+val BlueEnd = Color(0xFF79C3FD)
+
+val OrangeStart = Color(0xFFFF8400)
+val OrangeEnd = Color(0xFFFDA35F)
+
+val GreenStart = Color(0xFF11B114)
+val GreenEnd = Color(0xFF52DB59)
+
+val TextWhite = Color(0xffeeeeee)
+val DeepBlue = Color(0xff06164c)
+val ButtonBlue = Color(0xff505cf3)
+val DarkerButtonBlue = Color(0xff566894)
+val LightRed = Color(0xfffc879a)
+val AquaBlue = Color(0xff9aa5c4)
+val OrangeYellow1 = Color(0xfff0bd28)
+val OrangeYellow2 = Color(0xfff1c746)
+val OrangeYellow3 = Color(0xfff4cf65)
+val Beige1 = Color(0xfffdbda1)
+val Beige2 = Color(0xfffcaf90)
+val Beige3 = Color(0xfff9a27b)
+val LightGreen1 = Color(0xff54e1b6)
+val LightGreen2 = Color(0xff36ddab)
+val LightGreen3 = Color(0xff11d79b)
+val BlueViolet1 = Color(0xffaeb4fd)
+val BlueViolet2 = Color(0xff9fa5fe)
+val BlueViolet3 = Color(0xff8f98fd)
+
